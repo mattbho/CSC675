c28fcce883f3bb241f70a5d4b607bc7c1d9e552c
Matt Ho <mtthwbh@gmail.com>
2018-04-18 16:53:07 -0700

    First table

---
+++ CreateTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Squad_Unit(
+    INTEGER unit_id NOT NULL,
+    INTEGER squad_id,
+    POINT unit_position,
+    INTEGER stats_hp,
+    INTEGER stats_mov,
+    INTEGER stats_spd,
+    INTEGAR stats_def,
+    PRIMARY KEY (squad_id),
+    FOREIGN KEY(unit_id)
+)
